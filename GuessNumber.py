3d7a2ae6711b2159c305df0acec417d2f3aee363
6opoB <77076782+6opoB@users.noreply.github.com>
2021-03-22 22:53:50 +0300

    Update GuessNumber.py

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,13 +1,13 @@
 import random
 
-print('Введи диапазон, в котором компьютер загадает число/Enter the range in which the computer will guess the number :')
+print('Enter the range in which the computer will guess the number :')
 a, b, number = int(input()), int(input()), 0
 x = random.randint(a, b)
-print('А теперь попробуй угадать число между/Now try to guess the number between', a, 'и/and', b, ':')
+print('Now try to guess the number between', a, 'and', b, ':')
 while number != x:
     number = int(input())
     if number < x:
-        print('Низко берешь, товарищ! Попробуй-ка еще/Too low, mate! Try again:')
+        print('Too low, mate! Try again:')
     elif number > x:
-        print('Ого, слишком много! Ну-ка сначала/Wow, too much! Try once more:')
-print('Ура! Угадал! Ты Ванга!/Hurray! You did it! You're a seer!')
+        print('Wow, too much! Try once more:')
+print('Hurray! You did it! You are a seer!')
